50ad20db9f5c6c7c9df98532839846dd334d1d21
Narasimham <narasimham@mail.com>
2025-10-23 18:28:02 +0530

    Fix: Applied build configuration for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // content: [
+  // "./src/**/*.{html,js,jsx,ts,tsx}",
+  // ],
   content: [
-  "./src/**/*.{html,js,jsx,ts,tsx}",
-],
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./components/**/*.{html,js,jsx,ts,tsx}",
+    "./App.tsx"
+  ],
 
   theme: {
     extend: {
